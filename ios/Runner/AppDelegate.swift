1a6c8b3d4a1b67384ee87e18208013d84c9e60af
dariakolpakova <daka.kolp01@gmail.com>
2024-04-15 15:11:01 +0300

    ios settings: added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,23 +4,24 @@ import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      GeneratedPluginRegistrant.register(with: self)
-      
-      UNUserNotificationCenter.current().delegate = self
-      WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-          GeneratedPluginRegistrant.register(with: registry)
-      }
-      
-      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        UNUserNotificationCenter.current().delegate = self
+        
+        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+            GeneratedPluginRegistrant.register(with: registry)
+        }
+        WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "bpiID", frequency: NSNumber(value: 15 * 60))
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
     override func userNotificationCenter(
-            _ center: UNUserNotificationCenter,
-            willPresent notification: UNNotification,
-            withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-                completionHandler(.alert) // shows banner even if app is in foreground
-            }
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler(.alert) // shows banner even if app is in foreground
+        }
 }
